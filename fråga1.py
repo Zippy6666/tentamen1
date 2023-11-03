748f7c1429af48d0da10c62720d45ffa6fdc55ed
Zippy6666 <simperzippy@gmail.com>
2023-11-03 09:11:11 +0100

    fråga1

--- fråga1.py
+++ fråga1.py
@@ -1,5 +1,53 @@
+username_and_password = {
+    'KalleAnka': '1234asdf', 
+    'Mimmi_PIG': 'Hösten2023', 
+    'Jann3Langben': 'password', 
+    'Sebastianmentor': 'PyAi23Masters'
+}
+
+
+def login():
+    username = input("användarnamn: ")
+    password = input("lösenord: ")
+
+    if (username in username_and_password
+    and username_and_password[username] == password):
+        print("Woop woop, du är inloggad")
+
+
+def register():
+    username = input("användarnamn: ")
+
+    if username in username_and_password:
+        print("Användarnamnet är redan taget!")
+        return
+
+    password = input("lösenord: ")
+    if len(password) <= 6:
+        print("Lösenord måste vara längre än 6 karakterer!")
+        return
+
+    if password==input("Validera lösenord: "):
+        print("Nytt konto registrerat!")
+        username_and_password[username] = password
+
+
 def main():
-    pass
+    while True:
+        print('1. Logga in!')
+        print('2. Skapa ny användare!')
+        print('3. Avsluta')
+
+        choice = input('>>>')
+
+        if choice == '1':
+            login()
+        elif choice == '2':
+            register()
+        elif choice == '3':
+            break
+        else:
+            print('Felaktigt val')
 
 
 if __name__ == "__main__":
